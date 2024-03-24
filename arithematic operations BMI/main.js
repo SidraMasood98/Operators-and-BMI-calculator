faf87b012b56e6a0344cf41b3bbe2140a6e8dec3
Sidra Masood <160876510+SidraMasood98@users.noreply.github.com>
2024-03-24 08:03:00 +0500

    Add files via upload

---
+++ arithematic operations BMI/main.js
@@ -0,0 +1,35 @@
+//addition(+):
+var apple = 5;
+var banana = 3;
+var fruitChart = (apple + banana);
+console.log(fruitChart);
+//subtraction(-):
+var basket = 8;
+var orange = 2;
+var remainingFruits = (8 - 2);
+console.log(remainingFruits);
+//multiplication(*):
+var shopper = 4;
+var mango = 3;
+var totalMangoes = (4 * 3);
+console.log(totalMangoes);
+//Division(/):
+var pencils = 20;
+var children = 5;
+var eachChild = (20 / 5);
+console.log(eachChild);
+//Exponentiation(**):
+var num1 = 4;
+var num2 = 3;
+var power = (Math.pow(4, 3));
+console.log(power);
+//Modulus %:
+var cherry = 21;
+var plate = 5;
+var eachPlate = (21 % 5);
+console.log(eachPlate);
+//BMI calculator:
+var weightInKg = 56;
+var HeightInMeter = 1.61;
+var bmi = weightInKg / (HeightInMeter * HeightInMeter);
+console.log("your BMI is: ".concat(bmi));
---
+++ arithematic operations BMI/main.ts
@@ -0,0 +1,41 @@
+//addition(+):
+let apple: number = 5;
+let banana: number = 3;
+let fruitChart: number = (apple + banana);
+console.log(fruitChart);
+
+//subtraction(-):
+let basket: number = 8 ;
+let orange: number = 2;
+let remainingFruits : number = (8-2);
+console.log(remainingFruits);
+
+//multiplication(*):
+let shopper : number = 4;
+let mango : number = 3;
+let totalMangoes : number = (4*3);
+console.log(totalMangoes);
+
+//Division(/):
+let pencils : number = 20;
+let children : number = 5;
+let eachChild : number =(20/5);
+console.log(eachChild);
+
+//Exponentiation(**):
+let num1 : number = 4;
+let num2 : number = 3;
+let power : number = (4**3);
+console.log(power);
+
+//Modulus %:
+let cherry : number = 21;
+let plate : number = 5;
+let eachPlate : number = (21 % 5);
+console.log(eachPlate);
+
+//BMI calculator:
+let weightInKg = 56;
+let HeightInMeter = 1.61;
+let bmi = weightInKg/(HeightInMeter * HeightInMeter);
+console.log(`your BMI is: ${bmi}`);
